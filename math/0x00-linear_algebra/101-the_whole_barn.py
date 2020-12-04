efaa911e0179a21266ea3ae1b612c2a39a949a5f
mecomonteshbtn <1574@holbertonschool.com>
2020-12-04 07:39:26 -0500

    Fixing pycodestyle on 101 advanced task

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -4,19 +4,23 @@
 Created on Tue Dec  1 19:24:36 2020
 @author: Robinson Montes
 """
+import numpy as np
 
 
 def add_matrices(mat1, mat2):
     """
-    Function that adds two matrices.
-    Parameters:
+    Add two matrices.
+
+    Parameters
+    ----------
     - matrix (list of lists): matrix to calculate the sum.
-    Return:
+
+    Returns
+    -------
      The sum of the 2 matrices as a list.
     """
-
     if np.shape(mat1) != np.shape(mat1):
-    	return None
+        return None
 
     result = np.add(mat1, mat2)
     return result
