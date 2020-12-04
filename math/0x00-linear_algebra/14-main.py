243f02f5707596675c2c85976760c49406794fea
mecomonteshbtn <1574@holbertonschool.com>
2020-12-04 07:52:08 -0500

    Create the main file to call a function that adds 2 matrices

--- math/0x00-linear_algebra/14-main.py
+++ math/0x00-linear_algebra/14-main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 19:24:36 2020
+@author: Robinson Montes
+"""
 import numpy as np
 np_matmul = __import__('14-saddle_up').np_matmul
 
@@ -8,4 +12,3 @@ mat2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 mat3 = np.array([[7], [8], [9]])
 print(np_matmul(mat1, mat2))
 print(np_matmul(mat1, mat3))
-
