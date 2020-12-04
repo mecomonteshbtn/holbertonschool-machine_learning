a070ea8eb9d1c748b18428715e544d37469c4dae
mecomonteshbtn <1574@holbertonschool.com>
2020-12-03 19:31:55 -0500

    A Function that adds 2 matrix

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 19:24:36 2020
+@author: Robinson Montes
+"""
+
+
+def add_matrices(mat1, mat2):
+    """
+    Function that adds two matrices.
+    Parameters:
+    - matrix (list of lists): matrix to calculate the sum.
+    Return:
+     The sum of the 2 matrices as a list.
+    """
+    result = np.add(mat1, mat2)
+    return result
