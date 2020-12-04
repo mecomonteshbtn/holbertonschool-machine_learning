31bac3a1ab5ef68e7a4d7f8b45698a726cb2447a
mecomonteshbtn <1574@holbertonschool.com>
2020-12-04 07:49:11 -0500

    Fixing pycodestyle in task 0 to google style

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -4,7 +4,6 @@
 Created on Tue Dec  1 19:24:36 2020
 @author: Robinson Montes
 """
-import numpy as np
 
 
 def add_matrices(mat1, mat2):
@@ -12,11 +11,9 @@ def add_matrices(mat1, mat2):
     Add two matrices.
 
     Parameters
-    ----------
     - matrix (list of lists): matrix to calculate the sum.
 
     Returns
-    -------
      The sum of the 2 matrices as a list.
     """
     if np.shape(mat1) != np.shape(mat1):
