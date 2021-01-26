73ef8b3e68467ceb57a097b9055468b294a1dd13
mecomonteshbtn <1574@holbertonschool.com>
2021-01-26 11:46:35 -0500

    Write a function def build_model(nx, layers, activations, lambtha, keep_prob): that builds a neural network with the Keras library

---
+++ supervised_learning/0x06-keras/1-input.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 11:00:22 2021
+
+@author: Robinson Montes
+"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Function that builds a neural network with the Keras library
+    Arguments:
+     - nx is the number of input features to the network
+     - layers is a list containing the number of nodes in each layer
+        of the network
+     - activations is a list containing the activation functions used for
+        each layer of the network
+     - lambtha is the L2 regularization parameter
+     - keep_prob is the probability that a node will be kept for dropout
+    Returns:
+     The keras model
+    """
+    inputs = K.Input(shape=(nx,))
+    regularizer = K.regularizers.l2(lambtha)
+    outputs = K.layers.Dense(layers[0], input_shape=(nx,),
+                             activation=activations[0],
+                             kernel_regularizer=regularizer,
+                             name='dense')(inputs)
+
+    for layer in range(1, len(layers)):
+        dropout = K.layers.Dropout(rate=(1 - keep_prob))(outputs)
+        outputs = K.layers.Dense(layers[layer],
+                                 activation=activations[layer],
+                                 kernel_regularizer=regularizer,
+                                 name='dense_' + str(layer))(dropout)
+
+    model = K.models.Model(inputs=inputs, outputs=outputs)
+
+    return model
