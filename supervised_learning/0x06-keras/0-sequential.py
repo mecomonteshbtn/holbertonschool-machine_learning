68337900f214d89485adc173fd5156baa5230f00
mecomonteshbtn <1574@holbertonschool.com>
2021-01-26 11:08:27 -0500

    Write a function def build_model(nx, layers, activations, lambtha, keep_prob): that builds a neural network with the Keras library

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 11:00:22 2021
+
+@author: Robinson Montes
+"""
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import reguralizers
+from tensorflow.keras.layers import Dense, Dropout
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Function that that builds a neural network with the Keras library
+    Arguments:
+     - nx is the number of input features to the network
+     - layers is a list containing the number of nodes in each layer
+        of the network
+     - activations is a list containing the activation functions used for
+        each layer of the network
+     - lambtha is the L2 regularization parameter
+     - keep_prob is the probability that a node will be kept for dropout
+    Returns:
+     The keras model
+    """
+    model = Sequential()
+    regularizer = regularizers.l2(lambtha)
+    model.add(Dense(layers[0], input_shape=(nx,),
+                             activation=activations[0],
+                             kernel_regularizer=regularizer,
+                             name='dense'))
+
+    for layer in range(1, len(layers)):
+        dname = 'dense_' + str(layer)
+        model.add(Dropout(1 - keep_prob))
+        model.add(Dense(layers[layer],
+                                 activation=activations[layer],
+                                 kernel_regularizer=regularizer,
+                                 name=dname))
+
+    return model
