abe0aad343a91655ea3cb56b059cedc11650af6e
mecomonteshbtn <1574@holbertonschool.com>
2021-01-26 11:20:26 -0500

    Fixing import modules

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -27,10 +27,10 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     model.add(K.layers.Dense(layers[0], input_shape=(nx,),
                              activation=activations[0],
                              kernel_regularizer=regularizer,
-                             name='dense'))
+                             name='input_layer'))
 
     for layer in range(1, len(layers)):
-        dname = 'dense_' + str(layer)
+        dname = 'layer_' + str(layer)
         model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(layers[layer],
                                  activation=activations[layer],
