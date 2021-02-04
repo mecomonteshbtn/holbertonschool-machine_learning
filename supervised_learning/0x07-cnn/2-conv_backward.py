0f55535f45bc054a4a3de02a72e46ea5fc83033a
mecomonteshbtn <1574@holbertonschool.com>
2021-02-04 12:19:33 -0500

    Write a function def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'): that performs back propagation over a pooling layer of a neural network

---
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 1 11:00:42 2021
+
+@author: Robinson Montes
+"""
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """
+    Function that performs back propagation over a convolutional layer of a NN
+
+    Arguments:
+     - dZ is a numpy.ndarray of shape (m, h_new, w_new, c_new) containing
+        the partial derivatives with respect to the unactivated output of
+        the convolutional layer
+        * m is the number of examples
+        * h_new is the height of the output
+        * w_new is the width of the output
+        * c_new is the number of channels in the output
+
+     - - A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+        containing the output of the previous layer
+        * m is the number of examples
+        * h_prev is the height of the previous layer
+        * w_prev is the width of the previous layer
+        * c_prev is the number of channels in the previous layer
+     - W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing
+        the kernels for the convolution
+        * kh is the filter height
+        * kw is the filter width
+        * c_prev is the number of channels in the previous layer
+        * c_new is the number of channels in the output
+     - b is a numpy.ndarray of shape (1, 1, 1, c_new) containing
+        the biases applied to the convolution
+     - activation is an activation function applied to the convolution
+     - padding is a string that is either same or valid, indicating
+        the type of padding used
+     - stride is a tuple of (sh, sw) containing the strides for
+        the convolution
+        * sh is the stride for the height
+        * sw is the stride for the width
+
+    Returns:
+     The partial derivatives with respect to
+     the previous layer (dA_prev), the kernels (dW),
+     and the biases (db), respectively
+    """
+    m_z, h_new, w_new, c_newz = dZ.shape
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
+
+    if padding == 'valid':
+        ph = 0
+        pw = 0
+
+    if padding == 'same':
+        ph = np.ceil(max((h_prev - 1) * sh + kh - h_prev, 0) // 2)
+        pw = np.ceil(max((w_prev - 1) * sw + kw - w_prev, 0) // 2)
+        A_prev = np.pad(A_prev, pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                        mode='constant', constant_values=0)
+
+    dW = np.zeros(W.shape)
+    dA_prev = np.zeros(A_prev.shape)
+    db = np.zeros(b.shape)
+    db[:, :, 0, :] = np.sum(dZ, axis=(0, 1, 2))
+
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for cn in range(c_new):
+                    aux_W = W[:, :, :, cn]
+                    aux_dZ = dZ[i, h, w, cn]
+                    dA_prev[i,
+                            h * sh:kh + (h * sh),
+                            w * sw:kw + (w * sw)] += (aux_W * aux_dZ)
+                    dW[:, :, :, cn] += (A_prev[i, h * sh:kh + (h * sh),
+                                        w * sw:kw + (w * sw)] * aux_dZ)
+
+    _, h_dA, w_dA, _ = dA_prev.shape
+    dA_prev = dA_prev[:, ph:h_dA-ph, pw:w_dA-pw, :]
+    return dA_prev, dW, db
---
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 1 11:00:42 2021
+
+@author: Robinson Montes
+"""
+import numpy as np
+
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """
+    Function  that performs back propagation over a pooling layer of a NN
+
+    Arguments:
+     - dA is a numpy.ndarray of shape (m, h_new, w_new, c_new) containing
+        the partial derivatives with respect to the output of the pooling layer
+        * m is the number of examples
+        * h_new is the height of the output
+        * w_new is the width of the output
+        * c_new is the number of channels in the output
+     - A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c)
+        containing the output of the previous layer
+        * h_prev is the height of the previous layer
+        * w_prev is the width of the previous layer
+     - kernel_shape is a tuple of (kh, kw) containing the size of the kernel
+        for the pooling
+        * kh is the kernel height
+        * kw is the kernel width
+     - stride is a tuple of (sh, sw) containing the strides for
+        the convolution
+        * sh is the stride for the height
+        * sw is the stride for the width
+     - mode is a string containing either max or avg, indicating whether
+        to perform maximum or average pooling, respectively
+
+    Returns:
+     The partial derivatives with respect to the previous layer (dA_prev)
+    """
+
+    m, h_new, w_new, c_new = dA.shape
+
+    m, h_prev, w_prev, c = A_prev.shape
+
+    kh, kw = kernel_shape
+
+    sh, sw = stride
+
+    dx = np.zeros_like(A_prev)
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for cn in range(c_new):
+                    x = kh + h * sh
+                    y = kw + w * sw
+                    if mode == 'max':
+                        aux = A_prev[i, h*sh:x, w*sw:y, cn]
+                        mask = (aux == np.max(aux))
+                        dx[i, h*sh:x, w*sw:y, cn] += dA[i, h, w, cn] * mask
+                    if mode == 'avg':
+                        dx[i, h*sh:x, w*sw:y, cn] += dA[i, h, w, cn] / kh * kw
+
+    return dx
